94dbafc359eaf283e742a1c1f4e17d4cd65757f6
Tanvir Anjum Labir <33982784+Labir852@users.noreply.github.com>
2023-11-01 22:59:55 +0600

    grid items styles

--- src/App.js
+++ src/App.js
@@ -2,42 +2,80 @@ import * as React from 'react';
 import './App.css';
 // import { Box,ThemeProvider } from '@mui/system';
 import img1 from './assets/images/image-1.webp'
-import Grid from '@mui/material/Grid';
+import Grid from '@mui/material/Unstable_Grid2';
 import { experimentalStyled as styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 
 function App() {
   const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
+    backgroundColor: '#1A2027',
+    height:'150px',
+    width:'150px',
+    boxShadow:'2px 2px 2px 2px grey',
+    borderRadius:'5px',
   }));
   
   return (
     <div className="App">
-      {/* <ThemeProvider
-      theme={{
-        palette: {
-          primary: {
-            main: '#007FFF',
-            dark: 'rgba(18, 18, 20, 0.7)',
-          },
-        },
-      }}
-    > */}
-     <Box className="__main">
+      
+     <Box className="__main__">
       <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-        {Array.from(Array(6)).map((_, index) => (
-          <Grid xs={2} sm={4} md={4} key={index}>
-            <Item>xs=2</Item>
+        
+          
+            
+            <Grid xs={2} md={4} sm={4}>
+            <Item className='__item__'>
+              <img src={img1}  style ={{height:'150px',width:'150px'}} alt="img1" />
+            </Item>
+        </Grid>
+        <Grid xs={2} sm={4} md={4}>
+        <Item className='__item__'>
+              <img src={img1}  style ={{height:'150px',width:'150px'}} alt="img1" />
+            </Item>
+        </Grid>
+        <Grid xs={2} sm={4} md={4}>
+        <Item className='__item__'>
+              <img src={img1}  style ={{height:'150px',width:'150px'}} alt="img1" />
+            </Item>
+        </Grid>
+
+
+        <Grid xs={2} md={4} sm={4}>
+            <Item className='__item__'>
+              <img src={img1}  style ={{height:'150px',width:'150px'}} alt="img1" />
+            </Item>
+        </Grid>
+        <Grid xs={2} sm={4} md={4}>
+        <Item className='__item__'>
+              <img src={img1}  style ={{height:'150px',width:'150px'}} alt="img1" />
+            </Item>
+        </Grid>
+        <Grid xs={2} sm={4} md={4}>
+        <Item className='__item__'>
+              <img src={img1}  style ={{height:'150px',width:'150px'}} alt="img1" />
+            </Item>
+        </Grid>
+
+        <Grid xs={2} md={4} sm={4}>
+            <Item className='__item__'>
+              <img src={img1}  style ={{height:'150px',width:'150px'}} alt="img1" />
+            </Item>
+        </Grid>
+        <Grid xs={2} sm={4} md={4}>
+        <Item className='__item__'>
+              <img src={img1}  style ={{height:'150px',width:'150px'}} alt="img1" />
+            </Item>
+        </Grid>
+        <Grid xs={2} sm={4} md={4}>
+        <Item className='__item__'>
+              <img src={img1}  style ={{height:'150px',width:'150px'}} alt="img1" />
+            </Item>
+        </Grid>
           </Grid>
-        ))}
-      </Grid>
+        
+      
     </Box>
-    {/* </ThemeProvider> */}
     </div>
   );
 }
